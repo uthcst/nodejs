d166c299cdf77979f1033dba88de735b553830bf
tzigri <tzihgri@gmail.com>
2022-11-30 23:03:28 +0200

    added about

--- express3_static.js
+++ express3_static.js
@@ -9,7 +9,12 @@ app.use(express.static("./www"));
  app.get('/about', (req, res) => {
   res.send('about')
 })
- */var server = app.listen(4000, function () {  
+ */
+
+app.get("/about", function (req, res) {
+  res.send("About this wiki");
+});
+  var server = app.listen(4000, function () {  
   var host = server.address().address  
   var port = server.address().port  
  console.log("Express static listening at port %s", port)  
